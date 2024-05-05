8e14cf425d71ea55e8d41dbb982077665685b851
sultan-it <157215085+sultan-it@users.noreply.github.com>
2024-05-05 07:47:07 +0300

    Update h.py

--- h.py
+++ h.py
@@ -5,13 +5,10 @@ app = Flask(__name__)
 
 @app.route('/map')
 def map_redirect():
-    # استرجاع الطول من الAPI
     longitude = requests.get("https://Blynk.cloud/external/api/get?token=qIpqgPqk7Zpw7L6juManzZs7SujZSngS&V1").text
     print(longitude)
-    # استرجاع العرض من الAPI
     latitude = requests.get("https://Blynk.cloud/external/api/get?token=qIpqgPqk7Zpw7L6juManzZs7SujZSngS&V2").text
 
-    # تكوين رابط خرائط Google
     map_url = redirect(f"https://www.google.com/maps/search/?api=1&query={longitude},{latitude}")
     return map_url
 
